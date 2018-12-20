20388c51fdce9690aeda6c188b15053c921ccb47
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2018-12-20 11:02:57 -0500

    skimr data summary and maps added to markdown report; functions to check NAs, solo unique values, and numeric outliers

--- R/functions.R
+++ R/functions.R
@@ -13,3 +13,54 @@ download_raw_p2_data <- function(endpoints = p2_api_endpoints(),
   invisible(files)
 }
 
+# get country name for retrieving map outline
+get_country_name <- function(country){
+  if(country=="Ivory Coast"){
+    country <- "Cote d'Ivoire"
+  }
+  return(country)
+}
+
+# indentify NA cells
+get_na <- function(dat){
+  which(is.na(dat), arr.ind=TRUE) %>%
+    as_tibble() %>%
+    mutate(flag = "NA")
+}
+
+# identify solo unique values
+get_solo_char <- function(dat){
+  map_df(seq_along(dat), function(i){
+    col_dat <- dat %>% pull(i)
+    if(class(col_dat) == "character" & !all(is.na(col_dat))){
+      col_tbl <- table(col_dat) %>% as_tibble()
+      solo_vals <- col_tbl %>%
+        filter(n==1) %>%
+        mutate(col_dat = paste0("^", col_dat, "$")) %>%
+        pull(col_dat) %>%
+        paste(collapse = "|")
+      if(!solo_vals == ""){
+        solo_vals_match <-  grepl(solo_vals,  col_dat)
+        which_solo <- tibble(row = which(solo_vals_match==TRUE), col = i, flag = "unique value")
+        return(which_solo)
+      }
+    }
+  })
+}
+
+# numeric identify outliers
+get_outlier <- function(dat){
+  map_df(seq_along(dat), function(i){
+    col_dat <- dat %>% pull(i)
+    if(class(col_dat) == "numeric" & !all(is.na(col_dat)) & !grepl("Latitude|Longitude", colnames(dat[,i])) ){
+      outlier <- boxplot.stats(col_dat, coef = 2.5)$out # x < (25th perc - coef * iqr) | x > (75th perc + coef * iqr)
+      if(length(outlier)>0){
+        outlier_match <-  col_dat  %in% outlier
+        which_outlier <- tibble(row = which(outlier_match==TRUE), col = i, flag = "numeric outlier")
+        return(which_outlier)
+      }
+    }
+  })
+}
+
+
