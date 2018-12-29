dbf91a8ed8bc97976716e8479d2eec94e3caba62
Noam Ross <noam.ross@gmail.com>
2018-12-28 19:53:50 -0500

    Change build options on master branch

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,27 @@
 set -e
 set +x
 
-# Set git config information
-git config --global user.name "Noam Ross (Travis-CI)"
-git config --global user.email "ross@ecohealthalliance.org"
+if [ ${CIRCLE_BRANCH} == "master" ]
+then
+  # Set git config information
+  git config --global user.name "Noam Ross (Travis-CI)"
+  git config --global user.email "ross@ecohealthalliance.org"
 
-# Clone the gh-pages repository
-git clone -b outputs \
-  https://${GITHUB_PAT}@github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}.git \
-  to_deploy
+  # Clone the gh-pages repository
+  git clone -b outputs \
+    https://${GITHUB_PAT}@github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}.git \
+    to_deploy
 
-# Change to the gh-page clone book-output directory
-cd to_deploy
-rm *.*
+  # Change to the gh-page clone book-output directory
+  cd to_deploy
+  rm *.*
 
-# Copy generated output to the deploy directory
-cp -r ../outputs/* ./
+  # Copy generated output to the deploy directory
+  cp -r ../outputs/* ./
 
-# Add all files to the repo
-git add *
-git commit  --no-verify -a -m "Auto-generated outputs (${CIRCLE_BUILD_NUM}) [ci skip]" || true
-git push -qf origin outputs
+  # Add all files to the repo
+  git add *
+  git commit  --no-verify -a -m "Auto-generated outputs (${CIRCLE_BUILD_NUM}) [ci skip]" || true
+  git push -qf origin outputs
+
+fi
