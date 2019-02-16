c81bb190c5b5e0c72fc8f76b86c278992b047df6
Noam Ross <noam.ross@gmail.com>
2019-02-16 17:36:02 -0500

    Update sodium key

--- dropbox_upload.R
+++ dropbox_upload.R
@@ -15,3 +15,8 @@ if(Sys.getenv("CIRCLE_BRANCH") == "master") {
 }
 
 
+# db_token <- readRDS("token.rds")
+# key <-sodium::keygen()
+# key64 <- base64enc::base64encode(key)
+# zz <- cyphr::encrypt(saveRDS(db_token, "token.rds.encrypted"), key =  cyphr::key_sodium(base64decode(key64)))
+# key64
