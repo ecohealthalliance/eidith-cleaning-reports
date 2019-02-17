86305f1538f54bb34a3dbf58b38738703da5e340
Noam Ross <noam.ross@gmail.com>
2019-02-16 19:01:15 -0500

    Fix yaml

--- dropbox_upload.R
+++ dropbox_upload.R
@@ -6,17 +6,15 @@ library(sodium)
 library(base64enc)
 
 
-if(Sys.getenv("CIRCLE_BRANCH") == "master") {
-  token <- decrypt(readRDS("token.rds.encrypted"), key = key_sodium(base64decode(Sys.getenv("SODIUM_KEY"))))
-  to_upload <- list.files("outputs", pattern = "(html|xlsx)$", full.names = TRUE)
-  for (f in to_upload) {
-    drop_upload(f, path  = "shared/eidith-cleaning-reports-outputs/", dtoken = token, autorename = FALSE)
-  }
+token <- decrypt(readRDS("token.rds.encrypted"), key = key_sodium(base64decode(Sys.getenv("SODIUM_KEY"))))
+to_upload <- list.files("outputs", pattern = "(html|xlsx)$", full.names = TRUE)
+for (f in to_upload) {
+  drop_upload(f, path  = "shared/eidith-cleaning-reports-outputs/", dtoken = token, autorename = FALSE)
 }
 
 
-# db_token <- readRDS("token.rds")
-# key <-sodium::keygen()
-# key64 <- base64enc::base64encode(key)
-# zz <- cyphr::encrypt(saveRDS(db_token, "token.rds.encrypted"), key =  cyphr::key_sodium(base64decode(key64)))
-# key64
+  # db_token <- readRDS("token.rds")
+  # key <-sodium::keygen()
+  # key64 <- base64enc::base64encode(key)
+  # zz <- cyphr::encrypt(saveRDS(db_token, "token.rds.encrypted"), key =  cyphr::key_sodium(base64decode(key64)))
+  # key64
